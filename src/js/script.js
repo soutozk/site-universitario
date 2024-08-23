332039a054a27f4bd956bfb1a0d8cc549ee9cc22
soutozk <jgbrmix@gmail.com>
2024-08-23 14:11:18 -0300

    up

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ window.addEventListener("scroll", function () {
   const navbar = document.getElementById("navbar");
   if (window.scrollY > 50) {
     navbar.classList.remove("transparent");
+
     navbar.classList.add("solid");
   } else {
     navbar.classList.remove("solid");
